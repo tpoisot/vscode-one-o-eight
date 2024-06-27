c70e238cc17b3d608c7e83060aaffcd06dd325f6
tpoisot <timothee.poisot@umontreal.ca>
2024-06-27 13:23:24 -0400

    fix themes

--- make.jl
+++ make.jl
@@ -23,7 +23,6 @@ include(joinpath(@__DIR__, "lib", "cardmaker.jl"))
 theme_files = readdir(joinpath(@__DIR__, "schemes"); join = true)
 
 for theme_file in theme_files
-    @info theme_file
     # Read the theme as a JSON file
     theme = JSON.parsefile(theme_file)
 
@@ -31,6 +30,19 @@ for theme_file in theme_files
     structure!(theme)
     ui!(theme)
     accents!(theme)
+
+    # Info messages
+    println("#### Checking contrast for theme $(theme["name"])")
+    for color_idx in 1:5
+        c_contrast = contrast(theme["background"], theme["c$(color_idx)"])
+        s_contrast = contrast(theme["background"], theme["s$(color_idx)"])
+        if c_contrast < 7.0
+            println("\t /!\\ c$(color_idx) fails to meet WCAG2.1 AAA")
+        end
+        if s_contrast < 4.5
+            println("\t /!\\ s$(color_idx) fails to meet WCAG2.1 AA")
+        end
+    end
 
     rendered = render(template, template_data(theme))
     theme_json = JSON.parse(rendered)
